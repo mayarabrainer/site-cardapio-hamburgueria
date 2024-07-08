4da8daab5f7c624813ce1f893cb357542ee59830
Mayara Mayara <mayarabraineraguiar@gmail.com>
2024-07-08 14:09:35 -0300

    MMA Hamburgueria

---
+++ script.js
@@ -0,0 +1,207 @@
+const menu = document.getElementById("menu");
+const cartBtn = document.getElementById("cart-btn");
+const cartModal = document.getElementById("cart-modal");
+const cartItemsContainer = document.getElementById("cart-items");
+const cartTotal = document.getElementById("cart-total");
+const checkoutBtn = document.getElementById("checkout-btn");
+const closeModalBtn = document.getElementById("close-modal-btn");
+const cartCounter = document.getElementById("cart-count");
+const addressInput = document.getElementById("address");
+const addressWarn = document.getElementById("address-warn")
+
+let cart = [];
+
+cartBtn.addEventListener("click", function () {
+    updateCartModal();
+    cartModal.style.display = "flex"
+});
+
+cartModal.addEventListener("click", function (event) {
+    if(event.target === cartModal) {
+        cartModal.style.display = "none"
+    }
+});
+
+closeModalBtn.addEventListener("click", function () {
+    cartModal.style.display = "none"
+});
+
+
+menu.addEventListener("click", function(event) {
+    
+    let parentButton = event.target.closest(".add-to-cart-btn")
+
+    if(parentButton){
+        const name = parentButton.getAttribute("data-name")
+        const price = parseFloat(parentButton.getAttribute("data-price"))
+        addToCart(name, price)
+    }
+   
+});
+
+
+function addToCart(name, price) {
+    if (!isOpen) {
+        showMessage("Ops, o restaurante no momento está fechado!");
+        return;
+    }
+
+    const existingItem = cart.find(item =>item.name === name )
+
+    if(existingItem){
+    existingItem.quantity += 1;    
+    }else{
+        cart.push({
+        name,
+        price,
+        quantity: 1,
+        })
+
+    }
+
+        
+    updateCartModal()
+    showMessage("Item adicionado com sucesso!", "#32CD32");
+
+}
+
+
+
+function updateCartModal(){
+    if (!isOpen) {
+        showMessage("Ops, o restaurante no momento está fechado!", "#ef4444");
+        return;
+    }
+
+    cartItemsContainer.innerHTML = "";
+    let total = 0;
+
+    cart.forEach(item => {
+        const cartItemElement = document.createElement("div");
+        cartItemElement.classList.add("flex", "justify-between", "mb-6", "flex-col")
+
+        cartItemElement.innerHTML = `
+            <div class="flex items-center justify-between">
+
+                <div>
+                    <p class="font-bold">${item.name}</p>
+                    <p>Qtd: ${item.quantity}</p>
+                    <p class="font-medium mt-2">R$ ${item.price.toFixed(2)}</p>
+                </div>
+
+                
+                 <button class="remove-btn" data-name="${item.name}">
+                    Remover
+                </button>
+                
+            </div>
+        `
+        total += item.price * item.quantity;
+
+        cartItemsContainer.appendChild(cartItemElement)
+    })
+
+    cartTotal.textContent = total.toLocaleString("pt-br", {
+        style: "currency",
+        currency: "BRL"
+    });
+
+    cartCounter.innerHTML = cart.length;
+}
+
+
+cartItemsContainer.addEventListener("click", function (event){
+    if(event.target.classList.contains("remove-btn")){
+        const name = event.target.getAttribute("data-name")
+        
+        removeItemCart(name);
+    }
+})
+
+
+function removeItemCart(name){
+    const index = cart.findIndex(item => item.name === name);
+
+    if(index !== -1){
+        const item = cart[index];
+
+        if(item.quantity > 1){
+            item.quantity -= 1;
+            updateCartModal();
+            return;
+        }
+
+        cart.splice(index, 1);
+        updateCartModal();
+    }
+}
+
+
+addressInput.addEventListener("input", function(event){
+    let inputValue = event.target.value;
+
+    if(inputValue !== ""){
+        addressInput.classList.remove("border-red-500")
+        addressWarn.classList.add("hidden")
+    }
+})
+
+function showMessage(message, color) {
+    Toastify({
+        text: message,
+        duration: 5000,
+        close: true,
+        gravity: "top",
+        position: "right",
+        stopOnFocus: true,
+        style: {
+            background: color,
+        },
+    }).showToast();
+}
+
+checkoutBtn.addEventListener("click", function(){
+    if (!isOpen) {
+        showMessage("Ops, o restaurante no momento está fechado!", "#ef4444");
+        return;
+    }
+
+    if(cart.length === 0) return;
+    if(addressInput.value === ""){
+        addressWarn.classList.remove("hidden")
+        addressInput.classList.add("border-red-500")
+        return;
+    }
+
+    const cartItems = cart.map((item) => {
+        return (
+            ` ${item.name} Quantidade: (${item.quantity}) Preço: R$${item.price}`
+        )
+    }).join("")
+
+    const message = encodeURIComponent(cartItems)
+    const phone = "11914792104"
+
+    window.open(`https://wa.me/${phone}?text=${message} Endereço: ${addressInput.value}`, "_black")
+
+    cart = [];
+    updateCartModal();
+})
+
+
+function CheckRestauranteOpen(){
+    const data = new Date();
+    const hora = data.getHours();
+    return hora >= 13 && hora < 23;
+}
+
+const spanItem = document.getElementById("date-span")
+const isOpen = CheckRestauranteOpen();
+
+if(isOpen){
+    spanItem.classList.remove("bg-red-500")
+    spanItem.classList.add("bg-green-600")
+}else{
+    spanItem.classList.remove("bg-green-600")
+    spanItem.classList.add("bg-red-500")
+}
